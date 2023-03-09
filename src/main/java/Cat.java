b0ed5141d6aa67bd0244d44f74261c49d3369413
biocregen <zamorin.m@yandex.ru>
2023-03-09 18:47:22 +0300

    Исправление1

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -2,6 +2,14 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Cat {
+    private String name;
     public Cat() {
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
